4fb2a7598eef7ae1cc257c994c0c644437b0ca2d
agungmuliawan <agungmuliawan96@gmail.com>
2024-01-31 01:44:14 +0700

    belajar routes

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('blog', function () {
+//     return view('blog');
+// });
 
+Route::get('/chat', function () {
+    echo 'nama saya agung muliawan';
+});
+Route::redirect('/dari_sini', '/kesini', 301);
+Route::permanentRedirect('/dari_sini_kak', '/kesini');
+
+//Route::view('/blog', 'blog', ['name' => 'agung muliawan']);
+
+// Route::get('user/{id}', function ($id) {
+//     return 'user' . $id;
+// });
+
+// Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'posts' . $postId;
+// });
+
+Route::get('users/{name?}', function ($name = null) {
+    return $name;
+});
+Route::get('userku/{nameku?}', function ($nameku = 'agungmuliawan') {
+    return $nameku;
+});
