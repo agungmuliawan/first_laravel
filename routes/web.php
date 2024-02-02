ae488d35e7daab7a9be6d1aa9ce1953ffb513e43
agungmuliawan <agungmuliawan96@gmail.com>
2024-02-02 08:15:21 +0700

    contoh

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//cek
 // Route::get('blog', function () {
 //     return view('blog');
 // });
